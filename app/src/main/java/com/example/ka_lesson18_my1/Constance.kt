b92f48f0683e11b33ec8f4b64c45d324b4a48086
MikeKrysan <krysanmihasan@gmail.com>
2022-07-13 12:51:51 +0300

    first commit - lesson 18

---
+++ app/src/main/java/com/example/ka_lesson18_my1/Constance.kt
@@ -0,0 +1,16 @@
+package com.example.ka_lesson18_my1
+
+object Constance {
+    const val SIGN_IN_STATE = "sign_in"
+    const val SIGN_UP_STATE = "sign_up"
+    const val SIGN_STATE = "sign_state"
+    const val REQUEST_CODE_SIGN_IN = 1111
+    const val REQUEST_CODE_SIGN_UP = 2222
+
+    const val LOGIN = "login"
+    const val PASSWORD = "password"
+    const val NAME = "name"
+    const val NAME2 = "name2"
+    const val NAME3 = "name3"
+    const val AVATAR_ID = "avatar_id"
+}
---
+++ app/src/main/java/com/example/ka_lesson18_my1/MainActivity.kt
@@ -0,0 +1,82 @@
+package com.example.ka_lesson18_my1
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.core.view.isVisible
+import com.example.ka_lesson18_my1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var bindingClass:ActivityMainBinding
+    private var login:String = "empty"
+    private var password:String = "empty"
+    private var name:String = "empty"
+    private var name2:String = "empty"
+    private var name3:String = "empty"
+    private var avatarImageId:Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        bindingClass = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindingClass.root)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == Constance.REQUEST_CODE_SIGN_IN) {
+            val l = data?.getStringExtra(Constance.LOGIN)
+            val p = data?.getStringExtra(Constance.PASSWORD)
+            if(login == l && password == p) {
+                bindingClass.imAvatar.visibility = View.VISIBLE
+                bindingClass.imAvatar.setImageResource(avatarImageId)
+                val textInfo = "$name $name2 $name3"
+                bindingClass.tvInfo.text = textInfo
+                bindingClass.bHide.visibility = View.GONE
+                bindingClass.bExit.text = "Выйти"
+            } else {
+                bindingClass.imAvatar.visibility = View.VISIBLE
+                bindingClass.tvInfo.text = "Такого пользователся не существует!"
+                bindingClass.imAvatar.setImageResource(R.drawable.dulia)
+            }
+        } else if(requestCode == Constance.REQUEST_CODE_SIGN_UP) {
+            login = data?.getStringExtra(Constance.LOGIN)!!
+            password = data.getStringExtra(Constance.PASSWORD)!!
+            name = data.getStringExtra(Constance.NAME)!!
+            name2 = data.getStringExtra(Constance.NAME2)!!
+            name3 = data.getStringExtra(Constance.NAME3)!!
+            avatarImageId = data.getIntExtra(Constance.AVATAR_ID, 0)
+            bindingClass.imAvatar.visibility = View.VISIBLE
+            bindingClass.imAvatar.setImageResource(avatarImageId)
+            val textInfo = "$name $name2 $name3"
+            bindingClass.tvInfo.text = textInfo
+            bindingClass.bHide.visibility = View.GONE
+            bindingClass.bExit.text = "Выйти"
+        }
+    }
+
+    fun onClickSignIn(view:View) {
+        if(bindingClass.imAvatar.isVisible && bindingClass.tvInfo.text.toString() != "Такого пользователся не существует!") {
+            bindingClass.imAvatar.visibility = View.INVISIBLE
+            bindingClass.tvInfo.text = ""
+            bindingClass.bHide.visibility = View.VISIBLE
+            bindingClass.bExit.text = getString(R.string.sign_in)
+        } else {
+            val intent = Intent(this, SignInUpAct::class.java)
+            intent.putExtra(Constance.SIGN_STATE, Constance.SIGN_IN_STATE)
+            startActivityForResult(intent, Constance.REQUEST_CODE_SIGN_IN)
+        }
+
+    }
+
+    fun onClickSignUp(view:View) {
+        val intent = Intent(this, SignInUpAct::class.java)
+        intent.putExtra(Constance.SIGN_STATE, Constance.SIGN_UP_STATE)
+        startActivityForResult(intent, Constance.REQUEST_CODE_SIGN_UP)
+    }
+
+
+
+}
